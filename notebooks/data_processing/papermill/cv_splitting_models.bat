ed4d211751366ce7a44062866a1ec76ca9f7df98
Artem Poliakov <artem4250@gmail.com>
2024-11-13 21:36:23 +0200

    add: cv_splitting_models.bat, cv_splitting_types.bat

---
+++ notebooks/data_processing/papermill/cv_splitting_models.bat
@@ -0,0 +1,3 @@
+papermill "../raw/cv_splitting.ipynb" "../completed/cv_splitting_models.ipynb" ^
+--cwd "../../../" --autosave-cell-every 10 --log-output --no-progress-bar ^
+-p classification "models"
---
+++ notebooks/data_processing/papermill/cv_splitting_types.bat
@@ -0,0 +1,3 @@
+papermill "../raw/cv_splitting.ipynb" "../completed/cv_splitting_types.ipynb" ^
+--cwd "../../../" --autosave-cell-every 10 --log-output --no-progress-bar ^
+-p classification "types"
