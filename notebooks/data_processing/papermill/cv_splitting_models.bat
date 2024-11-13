2caa9f24de5d0de4f7faa177d2f140318e0ebb58
Artem Poliakov <artem4250@gmail.com>
2024-11-13 22:28:34 +0200

    update: cv_splitting_models.bat, cv_splitting_types.bat

--- notebooks/data_processing/papermill/cv_splitting_models.bat
+++ notebooks/data_processing/papermill/cv_splitting_models.bat
@@ -1,3 +1,4 @@
+call "../../../.venv/Scripts/activate"
 papermill "../raw/cv_splitting.ipynb" "../completed/cv_splitting_models.ipynb" ^
 --cwd "../../../" --autosave-cell-every 10 --log-output --no-progress-bar ^
 -p classification "models"
--- notebooks/data_processing/papermill/cv_splitting_types.bat
+++ notebooks/data_processing/papermill/cv_splitting_types.bat
@@ -1,3 +1,4 @@
+call "../../../.venv/Scripts/activate"
 papermill "../raw/cv_splitting.ipynb" "../completed/cv_splitting_types.ipynb" ^
 --cwd "../../../" --autosave-cell-every 10 --log-output --no-progress-bar ^
 -p classification "types"
