74aa43679db08b099bed12011e7cb42ff2d100d0
Artem Poliakov <artem4250@gmail.com>
2024-11-15 22:59:11 +0200

    rename & update: m1_models_cv1.bat -> m1.bat

--- notebooks/training/papermill/m1.bat
+++ notebooks/training/papermill/m1.bat
@@ -1,5 +1,5 @@
 call "../../../.venv/Scripts/activate"
 papermill "../raw/m1.ipynb" "../completed/m1_models_cv1.ipynb" ^
 --cwd "../../../" --autosave-cell-every 0 --log-output --no-progress-bar ^
--p notebook_classification "models" ^
--p notebook_cv 1
+-p notebook_classification %1 ^
+-p notebook_cv %2
